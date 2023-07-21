de16c57c4067008d15d709bdcf44829047895d2d
Anuran Roy <anuranroy02@gmail.com>
2023-07-21 18:25:45 +0530

    [ADD] Added Peewee DB data

---
+++ python/db/postgres/db.py
@@ -0,0 +1,5 @@
+from peewee import PostgresqlDatabase
+
+# Replace 'your_database_name', 'your_username', and 'your_password' with actual values
+db = PostgresqlDatabase(
+    'neondb', user='anuran-roy', password='UXTnG6Vm3RsC', host='ep-shy-sun-750855.us-west-2.aws.neon.tech', port=5432)
