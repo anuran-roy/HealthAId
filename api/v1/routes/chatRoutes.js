a2ba4247f0b0db82954985ea0699f12957ff634e
Abhishek Agarwal <abhishek.agarwal.work20@gmail.com>
2023-07-21 23:18:12 +0530

    get chat method changed

--- api/v1/routes/chatRoutes.js
+++ api/v1/routes/chatRoutes.js
@@ -4,6 +4,6 @@ import { getChatById, postMessage } from '../controllers/chatController.js';
 const chatRouter = express.Router();
 
 chatRouter.route('/msg').post(postMessage);
-chatRouter.route('/:id').post(getChatById);
+chatRouter.route('/:id').get(getChatById);
 
 export default chatRouter;
