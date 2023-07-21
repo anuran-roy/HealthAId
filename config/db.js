4c7ac65f96e2f40b71d0efc187f07b73cd751d52
Abhishek Agarwal <abhishek.agarwal.work20@gmail.com>
2023-07-21 10:36:21 +0530

    user and chat models added, db setup

---
+++ config/db.js
@@ -0,0 +1,17 @@
+import { connect, connection } from 'mongoose';
+const dbURL = process.env.MONGODB_URL;
+
+const connectDB = () => {
+  connect(dbURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  const db = connection;
+  db.on('error', console.error.bind(console, 'Connection error:'));
+  db.once('open', () => {
+    console.log('Database connected');
+  });
+};
+
+export default connectDB;
