6395a06c53e899711b32ab81c2d3b3d3488ccdfb
Abhishek Agarwal <abhishek.agarwal.work20@gmail.com>
2023-07-21 19:35:14 +0530

    chat model bug fix

--- api/v1/models/ChatModel.js
+++ api/v1/models/ChatModel.js
@@ -4,7 +4,7 @@ const { Schema, model } = mongoose;
 const chatSchema = new Schema(
   {
     userID: {
-      type: [Schema.Types.ObjectId],
+      type: Schema.Types.ObjectId,
       ref: 'user',
     },
     email: String,
