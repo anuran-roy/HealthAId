e58483f3a22842552cd782f8ef6019187f730144
Anuran Roy <anuranroy02@gmail.com>
2023-07-22 05:57:55 +0530

    [ADD] Added Qdrant Vector DB

--- python/db/qdrant/db.py
+++ python/db/qdrant/db.py
@@ -1,6 +1,9 @@
 from qdrant_client import QdrantClient
 
-qdrant = QdrantClient("http://localhost:6333") # Connect to existing Qdrant instance, for production
+qdrant = QdrantClient(
+    "http://localhost:6333"
+)  # Connect to existing Qdrant instance, for production
+
 
 def get_top_k():
     qdrant
