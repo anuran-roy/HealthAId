170b26e4c9589209658684277f6e45a4fdbc8004
Anuran Roy <anuranroy02@gmail.com>
2023-07-22 05:55:33 +0530

    [ADD] Added potential possible entrypoint for microservice architecture

--- python/llms/augmentation/mayoclinic_augmentation.py
+++
@@ -1,6 +0,0 @@
-from db.postgres.models import models
-from utils.embeddings import generate_chunk_embeddings
-
-def get_condition_from_mayoclinic(query: str) -> str:
-    embedding1 = generate_chunk_embeddings(query)
-    
