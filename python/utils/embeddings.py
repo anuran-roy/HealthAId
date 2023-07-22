285309564c8be0136afbf20e7b234c6a4bda21ea
Anuran Roy <anuranroy02@gmail.com>
2023-07-22 05:56:51 +0530

    [ADD] Added embedding utilities

--- python/utils/embeddings.py
+++ python/utils/embeddings.py
@@ -1,9 +1,23 @@
 from sentence_transformers import SentenceTransformer
+import numpy as np
+import tiktoken
+
+enc = tiktoken.get_encoding("cl100k_base")
 
 model = SentenceTransformer(
-    "all-MiniLM-L6-v2"
+    "bert-base-nli-stsb-mean-tokens"
 )  # Choosing MiniLM for the speed gains while retaining quality (Refer https://www.sbert.net/docs/pretrained_models.html)
+
+
+def chunk_text(text: str) -> list[str]:
+    return enc.encode(text)
 
 
 def generate_chunk_embeddings(text):
     return model.encode(text).tolist()
+
+
+def similarity_match(embedding_1, embedding_2) -> float:
+    return np.dot(embedding_1, embedding_2) / (
+        np.linalg.norm(embedding_1) * np.linalg.norm(embedding_2)
+    )
