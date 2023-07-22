575ab761910ddaaf284d7523ddefff37367e19fb
Anuran Roy <anuranroy02@gmail.com>
2023-07-22 05:56:26 +0530

    [ADD] Added pipeline utilities

---
+++ python/utils/pipeline.py
@@ -0,0 +1,41 @@
+from utils.elasticsearch import total_search, vector_search, send_to_elasticsearch
+from utils.embeddings import generate_chunk_embeddings
+from db.elasticsearch.db import client
+
+def get_data(query: str, source: str = "mayoclinic", threshold: float = 0.00313, limit: int=10) -> str:
+    query_embedding = generate_chunk_embeddings(query)
+
+    required_fields = [
+        "Name",
+        "Overview",
+        "Symptoms",
+        "Causes",
+        "Complications",
+        "Prevention",
+        "Related",
+        "Risk_factors",
+        "Types"
+    ]
+    results = total_search(content_vector=query_embedding, fields=required_fields, index=f"{source}_index", client=client, threshold=threshold, limit=limit)
+
+    print([x["_score"] for x in results])
+    relevant_results = list(map(lambda x: x["_source"], results))
+
+    print("Total results = ", len(results))
+    print("Relevant results = ", len(relevant_results))
+
+    joined_res = []
+    for relevant_result in relevant_results:
+        joined_data = ""
+        for key, value in relevant_result.items():
+            if key in required_fields:
+                joined_data += f"\n```{key}\n{value}\n```\n"
+        joined_res.append(joined_data)
+
+    final_str = "\n\n".join(joined_res).replace("\nNULL\n```", "\n```")
+
+    return final_str[:min(len(final_str), 1500)]
+
+if __name__ == "__main__":
+    data = get_data("I have a high temperature on my head.")
+    print(data[:200])
